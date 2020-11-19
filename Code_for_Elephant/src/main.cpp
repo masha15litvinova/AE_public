268c2233df691ecb84a0689ba2eb930ae66de3f3
mari223399 <admin@b0-robot-nb-02.sch239.net>
2020-11-19 20:37:16 +0300

    git push

---
+++ Code_for_Elephant/src/main.cpp
@@ -0,0 +1,89 @@
+#include <Arduino.h>
+
+#include <calibration.h>
+#define SERVO0_AVG 610
+#define SERVO1_AVG 615
+#define SERVO2_AVG 650
+#define SERVO3_AVG 600
+#define SERVO4_AVG 620
+#define SERVO5_AVG 650
+#define SERVO6_AVG 620
+#define SERVO7_AVG 600
+#define SERVO8_AVG 630
+#define SERVO9_AVG 650
+#define SERVO10_AVG 600
+#define SERVO11_AVG 200
+void setup()
+{
+  // interrupt # 0, pin 2
+ 
+  
+  pwm.begin(); //Объявление объекта pwm
+
+//  pwm.setOscillatorFrequency(27000000);
+ pwm.setPWMFreq(50);  // Analog servos run at ~50 Hz updates
+
+  delay(10);
+  Serial.begin(9600);
+
+  
+}
+
+// You can use this function if you'd like to set the pulse length in seconds
+// e.g. setServoPulse(0, 0.001) is a ~1 millisecond pulse width. It's not precise!
+
+
+
+
+void loop()
+{
+  int t = 0;
+  int enc = 0;
+  float f = 0;
+  float h = 0; 
+
+
+
+ /* while (abs(enc) < 300)
+  {
+      motor_lebedka(-100);
+   
+    Serial.println(enc);
+    t++;
+  delay(1);
+  }
+ */
+
+ 
+     /*while (enc > 0)
+  {
+    
+    motor_lebedka(80);
+ 
+    Serial.println(enc);
+  delay(1);
+  Enc();
+ 
+  }*/
+ Serial.println("test servo");
+ pwm.setPWM(0,0,SERVO0_AVG);
+ pwm.setPWM(1,0,SERVO1_AVG);
+ pwm.setPWM(2,0,SERVO2_AVG);
+ pwm.setPWM(3,0,SERVO3_AVG);
+ pwm.setPWM(4,0,SERVO4_AVG);
+ pwm.setPWM(5,0,SERVO5_AVG);
+ pwm.setPWM(6,0,SERVO6_AVG);
+ pwm.setPWM(7,0,SERVO7_AVG);
+ pwm.setPWM(8,0,SERVO8_AVG);
+ pwm.setPWM(9,0,SERVO9_AVG);
+ pwm.setPWM(10,0,SERVO10_AVG);
+ pwm.setPWM(11,0,SERVO11_AVG);
+ delay(100);
+ Serial.println("ready");
+// Serial.println("0 servo");
+// pwm.setPWM(1,0, 566);
+// Serial.println("1 servo");
+// pwm.setPWM(2,0, 565);
+// Serial.println("2 servo");
+// delay(2000);
+}
--- Code_for_Elephant/src/servomotors.h
+++ Code_for_Elephant/src/servomotors.h
@@ -1,6 +1,42 @@
 //Library to control servomotors
 #include <Adafruit_PWMServoDriver.h>
 #include <Wire.h>
+#define SERVO0_MIN 173
+#define SERVO0_MAX 963
+
+#define SERVO1_MIN 170
+#define SERVO1_MAX 962
+
+#define SERVO2_MIN 170
+#define SERVO2_MAX 960
+
+#define SERVO3_MIN 170
+#define SERVO3_MAX 964
+
+#define SERVO4_MIN 170
+#define SERVO4_MAX 964
+
+#define SERVO5_MIN 170
+#define SERVO5_MAX 960
+
+#define SERVO6_MIN 170
+#define SERVO6_MAX 960
+
+#define SERVO7_MIN 171
+#define SERVO7_MAX 965
+
+#define SERVO8_MIN 170
+#define SERVO8_MAX 960
+
+#define SERVO9_MIN 170
+#define SERVO9_MAX 962
+
+#define SERVO10_MIN 170
+#define SERVO10_MAX 960
+
+#define SERVO11_MIN 170
+#define SERVO11_MAX 960
+
 
 
 
