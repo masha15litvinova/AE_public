44a4fc486a41be9bfbf01cbd77bc5dc5446863c4
mari223399 <admin@b0-robot-nb-02.sch239.net>
2020-12-24 17:09:29 +0300

    edit my_test7

--- new_servos/my_test7/my_test7.ino
+++ new_servos/my_test7/my_test7.ino
@@ -8,10 +8,10 @@ ServoDriverSmooth servos[12];
 //uint32_t timer;//таймер для сервомотора
 
 //примерно то же, но несколько серв
-int MAX_theory[] = {963, 962, 960, 964, 964, 960, 960, 965, 960, 962, 960, 960};
-int MIN_theory[] = {173, 170, 170, 170, 170, 170, 171, 170, 170, 170, 170, 170};
-int MAX_real[] = {800, 800, 800, 800, 800, 800, 800, 800, 800, 800, 800, 800};
-int MIN_real[] = {200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200};
+int MAX_theory[] = {963, 962, 960, 964, 964, 960, 960, 965, 960, 962, 960, 960};   //теоретический максимум для самого сервомотора
+int MIN_theory[] = {173, 170, 170, 170, 170, 170, 171, 170, 170, 170, 170, 170};   //теоретический минимум для самого сервомотора
+int MAX_real[] = {800, 800, 800, 800, 800, 800, 800, 800, 800, 800, 800, 800};     //куда серва реально может повернуться, чтобы лапа не сломалась максимум
+int MIN_real[] = {200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200};     //куда серва реально может повернуться, чтобы лапа не сломалась минимум
 void count_average()
 {
   int AVG_theory[12];
@@ -32,7 +32,7 @@ void count_average()
     Serial.print(String(AVG_real[i]) + " ");
   }
 }
-void servos_begin()
+void servos_begin() //инициализация всех сервомоторов (минимум максимум)
 {
   count_average();
   Serial.println(" ");
@@ -46,7 +46,7 @@ void servos_begin()
   delay(100);
 }
 
-void set_servos_speeds(int *speeds_array)
+void set_servos_speeds(int *speeds_array) //задание скоростей всем 12 сервам через массив
 {
   Serial.println(" ");
   Serial.println("Speeds:");
@@ -58,7 +58,7 @@ void set_servos_speeds(int *speeds_array)
   
 
 }
-void set_servos_accels(int *accels_array)
+void set_servos_accels(int *accels_array) //задание ускорений всем 12 сервам через массив
 {
   Serial.println(" ");
   Serial.println("Accels:");
@@ -70,6 +70,25 @@ void set_servos_accels(int *accels_array)
   
 
 }
+void  servos_go(int *angles_array)
+{
+  Serial.println(" ");
+  Serial.println("Angles:");
+  for (byte i = 0; i < 12; i++)
+  {
+    Serial.print(String(angles_array[i]) + " ");
+    }
+   bool canRun = true;
+   while(canRun)
+   {
+    canRun = 1
+    servos_end_count = 0; //количество сервомоторо, чье движение закончено
+    for (byte i = 0; i < 12; i++)
+    {
+      
+      }
+    }
+  }
 
 /*void servos_go(int servos_angles[], int servos_speeds[],float servos_accels = [0,0,0,0,0,0,0,0,0,0,0,0]) //поворачивает до заданного угла с заданной скоростью и ускорением
   {
